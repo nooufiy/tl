c72378346c77dff1a8632cc0248a786ebebca003
noou <83199729+nooufiy@users.noreply.github.com>
2022-10-22 21:16:09 +0700

    Update map.js

--- map.js
+++ map.js
@@ -1,3 +1,5 @@
 Array.from(document.querySelectorAll('td>a')).map(function(x){return x.href});
 alert(Array.from(document.querySelectorAll('td>a')).map(function(x){return x.href}));
 alert(Array.from(document.querySelectorAll('td>a')).map(a => a.href.replace('https://baligreentour.com/','')));
+const dt = []; Array.from(document.querySelectorAll('td>a')).map(x => dt.push(x.href.replace('https://baligreentour.com/',''))); console.log(dt);
+const at = Array.from(document.querySelectorAll('td>a')).map(x => x.href.replace('https://baligreentour.com/',''));
