bfe5d7d89ae73e0c4517ae4c2c4fa2d6ac073b81
noou <83199729+nooufiy@users.noreply.github.com>
2022-10-22 10:19:11 +0700

    Update map.js

--- map.js
+++ map.js
@@ -1,2 +1,3 @@
 Array.from(document.querySelectorAll('td>a')).map(function(x){return x.href});
 alert(Array.from(document.querySelectorAll('td>a')).map(function(x){return x.href}));
+alert(Array.from(document.querySelectorAll('td>a')).map(a => a.href.replace('https://baligreentour.com/','')));
